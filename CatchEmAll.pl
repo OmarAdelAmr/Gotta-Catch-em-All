947e15773d0953004c711b9e281ed6001ec9ab8b
Moaaz Maamoon <moaaz.ali95@gmail.com>
2016-11-20 17:33:57 +0200

    Removed un-needed testing

--- CatchEmAll.pl
+++ CatchEmAll.pl
@@ -16,7 +16,6 @@ adjCell(X0, Y0, X1, Y1):- (X0 is X1 + 1, Y0 is Y1);
                           (X0 is X1, Y0 is Y1 + 1);
                           (X0 is X1, Y0 is Y1 - 1).
 
-test(Px, Py, Gx, Gy, S):- player(_, Px, Py, S), containsPokimon(Gx, Gy, S).
 player(Steps, X0, Y0, result(A,S)):- player(Steps1, X1, Y1, S),
                                                 (
                                                   (A = north, canMove(north, X1, Y1), X0 is X1, Y0 is Y1 - 1, Steps is Steps1 + 1);
@@ -25,17 +24,10 @@ player(Steps, X0, Y0, result(A,S)):- player(Steps1, X1, Y1, S),
                                                   (A = west, canMove(west, X1, Y1), Y0 is Y1,  X0 is X1 - 1, Steps is Steps1 + 1)
                                                   ).
 
-                                                  
 goal(Steps, X0, Y0, S):-  containsPokimon(1, 1, S), player(Steps, X0, Y0, S),  stepsToHatch(E, s0), Steps > E - 1.
 
-listOfPokimons([[2,2], [0,1], [1,1], [2,1]]).
-
-
-testOfTest(S, [Hx|Tx], [Hy|Ty]):- Hx == 0, Hy == 0 , test(Hx, Hy, S).
-
-test(X, Y, S):- player(_,X,Y, S), containsPokimon(X,Y,S).
-containsPokimon(X, Y, result(A, S)):-  hasPokimon(X, Y), player(_, X1, Y1, S),
-                                \+ (
+containsPokimon(X, Y, S):-  hasPokimon(X, Y), player(_, X1, Y1, result(A, S)),
+                                 (
                                   ( X1 is X, Y1 is Y + 1, (\+ A = north));
                                   ( X1 is X, Y1 is Y - 1, (\+ A = south));
                                   ( X1 is X + 1, Y1 is Y, (\+ A = west));
